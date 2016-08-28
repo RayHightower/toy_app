7324238d4bb75df6f72901d0c8100da4793f95ec
Raymond T. Hightower <rth@wisdomgroup.com>
2016-08-27 21:56:49 -0500

    Commit uncommitted changes before we make another round of changes so that we can avert chaos before it ensues!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render text: "Where will this text appears?\nAnd will the new line be rendered correctly?"
+  end
+
+  def goodbye
+    render html: "Goodbye, Universe!"
+  end
+  
 end
